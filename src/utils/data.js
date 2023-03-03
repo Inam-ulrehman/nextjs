63b49765ce27cba7138b86f2766750ff13f5973c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-03 16:32:04 -0500

    fix

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,12 +1,3 @@
-import {
-  FaHome,
-  FaEnvelope,
-  FaRegAddressBook,
-  FaClipboardList,
-  FaProductHunt,
-
-  // FaSignInAlt,
-} from 'react-icons/fa'
 import { icons } from './Icons'
 
 // ========logo =========== //
@@ -14,53 +5,47 @@ export const logo =
   'https://res.cloudinary.com/inam6530/image/upload/v1677861169/Inamwebsolutions-nextjs/Fresh_INAMWEBSOLUTIONS_qvfgi0.svg'
 // ========Nav Bar=========== //
 
-export const navbar = [
-  { id: 1, path: '/', title: 'Home', icon: <FaHome /> },
-  { id: 2, path: '/about', title: 'About Us', icon: <FaEnvelope /> },
-  { id: 4, path: '/portfolios', title: 'Portfolios', icon: <FaProductHunt /> },
-  { id: 5, path: '/services', title: 'Services', icon: <FaClipboardList /> },
-  {
-    id: 7,
-    path: '/contactus',
-    title: 'Contact Us',
-    icon: <FaRegAddressBook />,
-  },
-  { id: 7, path: '/dashboard', title: 'Dashboard', icon: <FaRegAddressBook /> },
-]
-
-export const featureDropDown = [
+export const portfolio = [
   {
     id: 1,
-    title: 'Autoscaling',
-    description:
-      'INAMWEBSOLUTIONS scales apps to meet user demand, automagically, based on load.',
-    icon: icons.scale,
+    title: 'Burger Queen',
+    image:
+      'https://res.cloudinary.com/inam6530/image/upload/v1669135918/inamwebsolutions/2_nerta3.svg',
+    link: 'https://burgerqueenbyinam.netlify.app/',
   },
   {
     id: 2,
-    title: 'usage_metrics',
-    description:
-      'Real-time metrics to debug issues. Slow query added? We’ll show you exactly where..',
-    icon: icons.activity,
+    title: 'Aryana Spa',
+    image:
+      'https://res.cloudinary.com/inam6530/image/upload/v1675258527/inamwebsolutions/5_p8gnj6.png',
+    link: 'https://aryanaspa.netlify.app/',
   },
   {
     id: 3,
-    title: 'production_ready',
-    description: 'All websites components are tested and production ready.',
-    icon: icons.flash,
+    title: 'Business Website',
+    image:
+      'https://res.cloudinary.com/inam6530/image/upload/v1669135917/inamwebsolutions/1_xtvk85.svg',
+    link: 'https://inamulrehman.netlify.app/',
   },
   {
     id: 4,
-    title: '+99% Uptime',
-    description:
-      'Applications stay on the grid with high availability and high uptime guarantees.',
-    icon: icons.server,
+    title: 'Real Pure Water',
+    image:
+      'https://res.cloudinary.com/inam6530/image/upload/v1675258527/inamwebsolutions/7_utcyd8.png',
+    link: 'https://www.realpurewater.ca/',
   },
   {
     id: 5,
-    title: 'supreme_support',
-    description:
-      'Overcome any challenge with a supporting team ready to respond.',
-    icon: icons.user,
+    title: 'K-W OPTICAL HOUSE',
+    image:
+      'https://res.cloudinary.com/inam6530/image/upload/v1675258527/inamwebsolutions/6_s4tfjv.png',
+    link: 'https://k-w-opticalhouse.netlify.app/',
+  },
+  {
+    id: 5,
+    title: 'BOLT SIGNS',
+    image:
+      'https://res.cloudinary.com/inam6530/image/upload/v1676298680/inamwebsolutions/portfolio_720_720_grrpv3.png',
+    link: 'https://boltsign.netlify.app/',
   },
 ]
