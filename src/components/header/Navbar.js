005cd744e23714fee9418b6f2f89f920bcd8b7d2
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-03 13:57:35 -0500

    fix

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,35 +1,65 @@
 import { Icons } from '@/styles/Icons'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Logo from './Logo'
+import NavbarDrawer from './NavbarDrawer'
+const initialState = {
+  showNavbarDrawer: false,
+}
 
 const Navbar = () => {
+  const [state, setState] = useState(initialState)
   const router = useRouter()
+
+  const handleNavbarDrawer = (e) => {
+    if (e.target.className === 'navbar-drawer-width') {
+      return
+    }
+    setState({ ...state, showNavbarDrawer: !state.showNavbarDrawer })
+  }
   return (
     <Wrapper>
       <Logo />
       <ul>
-        <li className={router.pathname === '/' && 'active'}>
+        <li className={router.pathname === '/' ? 'active' : ''}>
           <Link href={'/'}>Home</Link>
         </li>
-        <li className={router.pathname === '/portfolios' && 'active'}>
+        <li className={router.pathname === '/portfolios' ? 'active' : ''}>
           <Link href={'/portfolios'}>Portfolios{Icons.dropDown}</Link>
           <div className='dropdown'>Drop down</div>
         </li>
-        <li className={router.pathname === 'services' && 'active'}>
+        <li className={router.pathname === 'services' ? 'active' : ''}>
           <Link href={'/services'}>Services{Icons.dropDown}</Link>
           <div className='dropdown'>Drop down</div>
         </li>
-        <li className={router.pathname === '/about' && 'active'}>
+        <li className={router.pathname === '/about' ? 'active' : ''}>
           <Link href={'/about'}>About Us</Link>
         </li>
-        <li className={router.pathname === '/contact' && 'active'}>
+        <li className={router.pathname === '/contact' ? 'active' : ''}>
           <Link href={'/contact'}>Contact Us</Link>
         </li>
       </ul>
-      <i className='menu-icon'>{Icons.menu}</i>
+      <i onClick={handleNavbarDrawer} className='menu-icon'>
+        {Icons.menu}
+      </i>
+      <div
+        onClick={handleNavbarDrawer}
+        className='navbar-drawer'
+        style={{
+          width: `${state.showNavbarDrawer ? '100vw' : '00px'}`,
+        }}
+      >
+        <div
+          className='navbar-drawer-width'
+          style={{
+            width: `${state.showNavbarDrawer ? '300px' : '00px'}`,
+          }}
+        >
+          <NavbarDrawer />
+        </div>
+      </div>
     </Wrapper>
   )
 }
@@ -104,6 +134,25 @@ const Wrapper = styled.nav`
   @media (min-width: 620px) {
     .menu-icon {
       display: none;
+    }
+  }
+  /* navbar drawer */
+  .navbar-drawer {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    height: 100vh;
+    background-color: var(--primary-05);
+    @media (min-width: 620px) {
+      display: none;
+    }
+    .navbar-drawer-width {
+      background-color: var(--white);
+      height: 100vh;
+      position: absolute;
+      top: 0;
+      right: 0;
+      transition: var(--transition);
     }
   }
 `
---
+++ src/components/header/NavbarDrawer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavbarDrawer = () => {
+  return <Wrapper>NavbarDrawer</Wrapper>
+}
+
+const Wrapper = styled.div``
+export default NavbarDrawer
