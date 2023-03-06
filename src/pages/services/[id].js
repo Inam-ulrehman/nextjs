a2ffe24ee6b95f329b32836cba0e3d850ed3751e
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-06 12:54:35 -0500

    fix

--- src/pages/services/[id].js
+++ src/pages/services/[id].js
@@ -10,7 +10,7 @@ const SingleService = ({ data }) => {
     <>
       <Head>
         <title>{data?.title}</title>
-        <meta name='description' content={data.description} />
+        <meta name='description' content={data?.description} />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
