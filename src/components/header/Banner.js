ab67b0bf02ab1a86488744910ac53af9b66555d6
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-06 15:08:06 -0500

    fix

--- src/components/header/Banner.js
+++ src/components/header/Banner.js
@@ -4,13 +4,15 @@ import styled from 'styled-components'
 const Banner = () => {
   return (
     <Wrapper>
-      <div className='contact'></div>
-      <div className='social'></div>
+      <div className='contact'>Contact</div>
+      <div className='social'>Social</div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
   background-color: var(--grey-9);
   color: var(--white);
   @media (max-width: 620px) {
