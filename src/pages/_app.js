2ea66769aedf9ec65ae28e713a5a954fffad1b68
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-14 07:37:11 -0400

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,12 +5,14 @@ import store from '@/store'
 import Layout from '@/components/layout'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
       <ToastContainer />
     </Provider>
