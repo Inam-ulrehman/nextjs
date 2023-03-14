3a856d7c2bed869f0220cdbb658db2ab308b9dc1
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-14 14:04:13 -0400

    fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,3 +5,6 @@ import axios from 'axios'
 export const customFetch = axios.create({
   baseURL: 'https://www.inamwebsolutions.com/api/v1/',
 })
+export const customFetchLocal = axios.create({
+  baseURL: 'http://localhost:3000/api/v1/',
+})
