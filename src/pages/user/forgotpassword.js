ee0b2285fcf401ac5d9866c8f40174efd85806dc
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-15 12:09:57 -0400

    fix

--- src/pages/user/forgotpassword.js
+++ src/pages/user/forgotpassword.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import FormInput from '@/components/FormInput'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
-const ForgotPassword = () => {
-  return <div>ForgotPassword</div>
+const initialState = {
+  email: '',
+  isLoading: false,
 }
+const ForgotPassword = () => {
+  const [state, setState] = useState(initialState)
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setState({ ...state, [name]: value })
+  }
+  return (
+    <Wrapper>
+      <div className='image-container'></div>
+      <div className='form-container'>
+        <form onSubmit={handleSubmit} className='form'>
+          {/* email */}
+          <FormInput
+            type='email'
+            name='email'
+            value={state.email}
+            onChange={handleChange}
+          />
+          <div className='btn-container'>
+            <Link href={'/user/login'} className='btn btn-a'>
+              Login / Register
+            </Link>
+            <button type='submit' className='btn' disabled={state.isLoading}>
+              {state.isLoading && <span className='loading-span'></span>}
+              <span className='btn-span'>Send Email</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.div`
+  .btn-container {
+  }
+`
 export default ForgotPassword
