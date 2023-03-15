76092455dc0194669772cba9f79f5f5b5aff8e4b
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-15 13:38:23 -0400

    fix

---
+++ src/pages/api/v1/users/email-link-forgot-password.js
@@ -0,0 +1,40 @@
+import dbConnect from '@/lib/dbConnect'
+import { UnauthenticatedError } from '@/lib/errors'
+import EmailLinkForgotPassword from '@/lib/sendgrid/EmailLinkForgotPassword'
+import Users from '@/models/User'
+import { StatusCodes } from 'http-status-codes'
+import { v4 as uuidv4 } from 'uuid'
+
+export default async function handler(req, res) {
+  await dbConnect()
+  const { method, body, query } = req
+
+  if (method === 'POST') {
+    const { email } = body
+    const user = await Users.findOne({ email })
+    if (!user) {
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: 'No user found.' })
+    }
+    const forgotPasswordId = uuidv4()
+    const updateUser = await Users.findOneAndUpdate(
+      { email },
+      { forgotPasswordId }
+    )
+    const emailResult = await EmailLinkForgotPassword({
+      email,
+      forgotPasswordId,
+    })
+    if (emailResult.msg === 'success') {
+      return res
+        .status(StatusCodes.OK)
+        .json({ msg: `Reset password link is sent to ${email}` })
+    }
+    if (emailResult.msg === 'error') {
+      return res
+        .status(StatusCodes.BAD_GATEWAY)
+        .json({ msg: 'Email server is down.' })
+    }
+  } else {
+    return res.status(StatusCodes.OK).json({ msg: 'Route Does not exist' })
+  }
+}
