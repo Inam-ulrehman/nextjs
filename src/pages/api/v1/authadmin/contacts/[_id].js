ed96e2256e3112738483208f88a265d52d40bbfc
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-18 08:17:25 -0400

    fix

---
+++ src/pages/api/v1/authadmin/contacts/[_id].js
@@ -0,0 +1,30 @@
+import dbConnect from '@/lib/dbConnect'
+import mongooseErrorHandler from '@/lib/mongoose-error-handler'
+import Contact from '@/models/Contact'
+import { StatusCodes } from 'http-status-codes'
+
+export default async function handler(req, res) {
+  await dbConnect()
+  const { method, body, query } = req
+
+  // Get a Sample
+  if (method === 'GET') {
+    try {
+      const result = await Contact.findById(query)
+      if (!result) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ msg: 'No record match' })
+      }
+      return res.status(StatusCodes.OK).json({ msg: 'success', result })
+    } catch (error) {
+      return mongooseErrorHandler(error, res)
+    }
+
+    return res.status(200).json({ name: 'Get your data' })
+  }
+  // Create a Sample
+  if (method === 'POST') {
+    return res.status(200).json({ name: 'Post your data' })
+  }
+}
