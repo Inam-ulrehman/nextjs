9c57c3fb015b3a1c78255df441b9ce367decc751
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-22 10:15:10 -0400

    fix

--- src/pages/api/v1/authadmin/images/upload.js
+++ src/pages/api/v1/authadmin/images/upload.js
@@ -1,46 +1,57 @@
-import nextConnect from 'next-connect'
+import dbConnect from '@/lib/dbConnect'
+import mongooseErrorHandler from '@/lib/mongoose-error-handler'
+import Users from '@/models/User'
+import { StatusCodes } from 'http-status-codes'
 import multer from 'multer'
-import { v4 as uuidv4 } from 'uuid'
-
-let filename = uuidv4() + '-' + new Date().getTime()
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: './public/uploads', // destination folder
-    filename: (req, file, cb) => cb(null, getFileName(file)),
-  }),
-})
-
-const getFileName = (file) => {
-  filename +=
-    '.' +
-    file.originalname.substring(
-      file.originalname.lastIndexOf('.') + 1,
-      file.originalname.length
-    )
-  return filename
-}
-
-const apiRoute = nextConnect({
-  onError(error, req, res) {
-    res
-      .status(501)
-      .json({ error: `Sorry something Happened! ${error.message}` })
-  },
-  onNoMatch(req, res) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
-  },
-})
-
-apiRoute.use(upload.array('file')) // attribute name you are sending the file by
-
-apiRoute.post((req, res) => {
-  res.status(200).json({ data: `/uploads/${filename}` }) // response
-})
-
-export default apiRoute
-
+const cloudinary = require('cloudinary').v2
+const upload = multer({ dest: 'uploads/' })
+const fs = require('fs')
 export const config = {
   api: {
-    bodyParser: false, // Disallow body parsing, consume as stream
+    bodyParser: false,
   },
+}
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+})
+export default async function handler(req, res) {
+  dbConnect()
+  const { method, body, query, headers } = req
+  const { userid, name } = headers
+
+  if (method === 'POST') {
+    console.log('post hit')
+    const middleware = upload.single('file')
+    middleware(req, res, async () => {
+      if (req.file === undefined) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ msg: `File name must be 'file'` })
+      }
+      const path = req.file.path
+      try {
+        const result = await cloudinary.uploader.upload(path, {
+          use_filename: true,
+          unique_filename: false,
+          folder: 'next/testing',
+          width: '720',
+          height: '720',
+          crop: 'fill',
+          // background_removal: 'cloudinary_ai',
+        })
+        fs.unlinkSync(path)
+        return res
+          .status(StatusCodes.ACCEPTED)
+          .json({ msg: 'file is uploaded', result })
+      } catch (error) {
+        fs.unlinkSync(path)
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ msg: 'uploading error by cloudinary', result: error })
+      }
+    })
+  }
 }
