aaa421f48104b665c0a2a404d6d1bb5719d3417d
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-22 09:49:35 -0400

    fix

--- src/pages/api/v1/authadmin/images/upload.js
+++ src/pages/api/v1/authadmin/images/upload.js
@@ -1,11 +1,11 @@
 import dbConnect from '@/lib/dbConnect'
-import mongooseErrorHandler from '@/lib/mongoose-error-handler'
-import Users from '@/models/User'
 import { StatusCodes } from 'http-status-codes'
 import multer from 'multer'
+import nextConnect from 'next-connect'
+import { v4 as uuidv4 } from 'uuid'
 const cloudinary = require('cloudinary').v2
-const upload = multer({ dest: 'public/' })
 const fs = require('fs')
+
 export const config = {
   api: {
     bodyParser: false,
@@ -17,6 +17,45 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 })
+
+// ========File upload next js Local Server =========
+let filename = uuidv4() + '-' + new Date().getTime()
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/uploads/profiles', // destination folder
+    filename: (req, file, cb) => cb(null, getFileName(file)),
+  }),
+})
+
+const getFileName = (file) => {
+  filename +=
+    '.' +
+    file.originalname.substring(
+      file.originalname.lastIndexOf('.') + 1,
+      file.originalname.length
+    )
+  return filename
+}
+
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res
+      .status(501)
+      .json({ error: `Sorry something Happened! ${error.message}` })
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
+  },
+})
+
+apiRoute.use(upload.array('file')) // attribute name you are sending the file by
+
+apiRoute.post((req, res) => {
+  res.status(200).json({ data: `/uploads/profiles/${filename}` }) // response
+})
+
+// ==============File upload local server end here ================
+
 export default async function handler(req, res) {
   dbConnect()
   const { method, body, query, headers } = req
