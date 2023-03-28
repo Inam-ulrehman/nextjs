1fa005e015222d9390d1f245316884e3b7960ad7
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-28 08:29:53 -0400

    fix

--- src/pages/api/v1/authadmin/blogs/index.js
+++ src/pages/api/v1/authadmin/blogs/index.js
@@ -2,7 +2,7 @@ import dbConnect from '@/lib/dbConnect'
 import mongooseErrorHandler from '@/lib/mongoose-error-handler'
 import Blogs from '@/models/Blog'
 import User from '@/models/User'
-import axios from 'axios'
+
 import { StatusCodes } from 'http-status-codes'
 
 export default async function handler(req, res) {
@@ -23,8 +23,7 @@ export default async function handler(req, res) {
         author,
         createdBy: userId,
       })
-      // VERCEL BUILD HOOK
-      await axios.post(process.env.VERCEL_DEPLOY_HOOK)
+
       return res
         .status(StatusCodes.CREATED)
         .json({ msg: 'Blog is created', result: blog })
--- src/pages/api/v1/authadmin/verceldeploy.js
+++ src/pages/api/v1/authadmin/verceldeploy.js
@@ -1,6 +1,7 @@
 import dbConnect from '@/lib/dbConnect'
 import mongooseErrorHandler from '@/lib/mongoose-error-handler'
 import Users from '@/models/User'
+import axios from 'axios'
 import { StatusCodes } from 'http-status-codes'
 export default async function handler(req, res) {
   await dbConnect()
@@ -14,12 +15,18 @@ export default async function handler(req, res) {
     }
     return res.status(StatusCodes.OK).json({ msg: 'success', result: user })
   }
-
+  // =========Vercel =========
   if (method === 'POST') {
     try {
-      return res.status(StatusCodes.OK).json({ msg: 'post request' })
+      const result = await axios.post(process.env.VERCEL_DEPLOY_HOOK)
+      console.log(result.data.job.state)
+      return res
+        .status(StatusCodes.OK)
+        .json({ msg: 'success', result: result.data })
     } catch (error) {
-      return mongooseErrorHandler(error, res)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: 'error', result: error })
     }
   }
 }
