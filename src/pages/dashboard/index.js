982eb37fb48761d35694a5d41ff3b916bb7296f6
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-28 10:21:21 -0400

    fix

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,9 @@
-import dbConnect from '@/lib/dbConnect'
-import Users from '@/models/User'
 import Head from 'next/head'
 import React from 'react'
-import * as jose from 'jose'
 import DashboardLayout from '@/components/dashboard/dashboard-layout'
 import { vercelDeploy } from '@/utils/axios'
 
-const Dashboard = ({ user }) => {
-  // console.log(user)
+const Dashboard = () => {
   return (
     <>
       <Head>
@@ -26,26 +22,5 @@ const Dashboard = ({ user }) => {
 Dashboard.getLayout = (page) => {
   return <DashboardLayout>{page}</DashboardLayout>
 }
-/* Retrieves pet(s) data from mongodb database */
-export async function getServerSideProps(context) {
-  await dbConnect()
-  const jwt = context.req.cookies['token']
 
-  const { payload } = await jose.jwtVerify(
-    jwt,
-    new TextEncoder().encode(process.env.JWT_SECRET)
-  )
-  const user = await Users.findById(
-    payload.userId,
-    '-password -forgotPasswordId -uuid '
-  )
-  // console.log(user)
-  // const users = result.map((doc) => {
-  //   const user = doc.toObject()
-  //   user._id = user._id.toString()
-
-  //   return user
-  // })
-  return JSON.parse(JSON.stringify({ props: { user } }))
-}
 export default Dashboard
