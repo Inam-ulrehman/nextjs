043ce556aab4fefe6dcc2f6f4c6389a7ad011f94
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-29 08:45:23 -0400

    fix

--- src/components/dashboard/blog/BlogDesign.js
+++ src/components/dashboard/blog/BlogDesign.js
@@ -4,7 +4,7 @@ import { CldImage } from 'next-cloudinary'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-const defaultImage = 'Inamwebsolutions-nextjs/website_blogs_zlhiwz'
+
 const initialState = {
   images: [],
 }
@@ -37,28 +37,22 @@ const BlogDesign = ({ blogs, readMore }) => {
         <div className='name-time'>
           <div className='name'>
             <span>Written By :</span>
-            <span> {!author ? 'inam' : author}</span>
+            <span> {author}</span>
           </div>
           <div className='time'>
-            <span>Posted On :</span>{' '}
-            <span>
-              {createdAt.length > 0
-                ? formatDate(createdAt)
-                : formatDate(new Date())}
-            </span>
+            <span>Posted On :</span>
+            {createdAt && <span>{formatDate(createdAt)}</span>}
           </div>
         </div>
         <div className='image-container'>
-          <CldImage
-            width={1200}
-            height={600}
-            alt={heading}
-            src={
-              state.images.length === 0
-                ? defaultImage
-                : state.images[0]?.public_id
-            }
-          ></CldImage>
+          {state.images.length > 0 && (
+            <CldImage
+              width={1200}
+              height={600}
+              alt={heading}
+              src={state.images[0]?.public_id}
+            ></CldImage>
+          )}
         </div>
         <span className='description'>
           {description}{' '}
