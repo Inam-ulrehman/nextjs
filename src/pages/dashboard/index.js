f5c594083c00c21f3278383060d038dd95d0c354
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-29 09:15:48 -0400

    test

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,21 +1,19 @@
+import dbConnect from '@/lib/dbConnect'
+import Users from '@/models/User'
 import Head from 'next/head'
 import React from 'react'
+import * as jose from 'jose'
 import DashboardLayout from '@/components/dashboard/dashboard-layout'
-import { vercelDeploy } from '@/utils/axios'
 
-const Dashboard = () => {
-  //
+const Dashboard = ({ user }) => {
+  // console.log(user)
   return (
     <>
       <Head>
         <title>Welcome to your dashboard</title>
         <meta name='description' content='Your dashboard page.' />
       </Head>
-      <div>
-        <button onClick={() => vercelDeploy()} className='btn' type='button'>
-          Vercel build
-        </button>
-      </div>
+      <div>Welcome to your dashboard</div>
     </>
   )
 }
@@ -23,5 +21,26 @@ const Dashboard = () => {
 Dashboard.getLayout = (page) => {
   return <DashboardLayout>{page}</DashboardLayout>
 }
+/* Retrieves pet(s) data from mongodb database */
+export async function getServerSideProps(context) {
+  await dbConnect()
+  const jwt = context.req.cookies['token']
 
+  const { payload } = await jose.jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  )
+  const user = await Users.findById(
+    payload.userId,
+    '-password -forgotPasswordId -uuid '
+  )
+  // console.log(user)
+  // const users = result.map((doc) => {
+  //   const user = doc.toObject()
+  //   user._id = user._id.toString()
+
+  //   return user
+  // })
+  return JSON.parse(JSON.stringify({ props: { user } }))
+}
 export default Dashboard
