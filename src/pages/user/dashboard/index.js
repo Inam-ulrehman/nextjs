2865b41333735b32a7825a264921bd72ebca4805
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-03-30 12:51:54 -0400

    fix

--- src/pages/user/dashboard/index.js
+++ src/pages/user/dashboard/index.js
@@ -1,7 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
+import styled from 'styled-components'
 
 const User = () => {
-  return <div>User</div>
+  return (
+    <Wrapper>
+      <div className='title'>
+        <h1>No active Services </h1>
+        <div className='title-underline'></div>
+        <Link className='btn btn-a' href={'/contact'}>
+          Contact us
+        </Link>
+      </div>
+    </Wrapper>
+  )
 }
 
+const Wrapper = styled.div`
+  min-height: calc(100vh - 94px);
+  .btn {
+    margin-top: 2rem;
+  }
+`
 export default User
